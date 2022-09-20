ab9ff92c44a09098a19c6b5ba8cec82d59e1c913
faridasad <asadlifaridd@gmail.com>
2022-09-20 22:56:59 +0400

    Add router

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,13 @@
 import styles from "../styles/Header.module.scss"
+import { useRouter } from "next/router"
 
 export default function Header() {
+
+    const router = useRouter()
+
     return(
         <header className={styles.header}>
-            <h3>Moviezz</h3>
+            <h3 onClick={() => router.push('/')}>Moviezz</h3>
             <nav>
                 <ul>
                     <li>I feel lucky</li>
